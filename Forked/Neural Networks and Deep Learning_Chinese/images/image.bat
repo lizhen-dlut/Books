633d2f0dceefa2146b15b2210620f20521e15114
hiatcg <hiatcg@gmail.com>
2017-08-25 09:45:58 +0800

    Fork Neural Networks and Deep Learning_Chinese

---
+++ Forked/Neural Networks and Deep Learning_Chinese/images/image.bat
@@ -0,0 +1,17 @@
+xelatex ti_graph-4.tex
+xelatex simple_model.eps softmax.tex
+xelatex softmax-1.tex
+xelatex softmax-2.tex
+xelatex softmax-3.tex
+xelatex softmax-4.tex
+xelatex step.tex
+xelatex step_function.tex
+xelatex step_parameterization.tex
+xelatex tanh_function.tex
+xelatex The_Hubble_eXtreme_Deep_Field.jpg the_two_towers.tex
+xelatex ti_graph.tex
+xelatex ti_graph_redux.tex
+xelatex ti_graph-0.tex
+xelatex ti_graph-1.tex
+xelatex ti_graph-2.tex
+xelatex ti_graph-3.tex
